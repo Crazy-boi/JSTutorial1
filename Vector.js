aafbb8a426107e07688fc2f54fc8b4273c0d7008
Crazy-boi <legolas3579@gmail.com>
2021-02-17 21:16:00 +0100

    Add Vector.js
    Position and movement are now calculated with Vector objects

--- Vector.js
+++ Vector.js
@@ -1,9 +1,11 @@
 //window.myNameSpace = window.myNameSpace || { };
 
+// LOOK AT https://gist.github.com/winduptoy/a1aa09c3499e09edbd33 WHEN MORE ADVANCED
+
 class Vector {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        this.x = parseFloat(x) || 0;
+        this.y = parseFloat(y) || 0;
     }
 
     angle() {
@@ -19,13 +21,76 @@ class Vector {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
 
-    add(v) {
-        if (v instanceof Vector) {
-
-        }
-    }
     add(x, y) {
         this.x += x;
         this.y += y;
+    }
+    /*getAdd(v) {
+        if (v instanceof Vector) {
+            return new Vector(this.x + v.x, this.y + v.y);
+        }
+    }*/
+    getAdd(x, y) {
+        return new Vector(this.x + x, this.y + y);
+    }
+
+    sub(x, y) {
+        this.x -= x;
+        this.y -= y;
+    }
+    /*getSub(v) {
+        if (v instanceof Vector) {
+            return new Vector(this.x - v.x, this.y - v.y);
+        }
+    }*/
+    getSub(x, y) {
+        return new Vector(this.x - x, this.y - y);
+    }
+
+    mult(s) {
+        this.x *= s;
+        this.y *= s;
+    }
+    getMult(s) {
+        return new Vector(this.x * s, this.y * s);
+    }
+
+    div(s) {
+        this.x /= s;
+        this.y /= s;
+    }
+    divXY(s1, s2) {
+        this.x /= s1;
+        this.y /= s2;
+    }
+    getDiv(s) {
+        return new Vector(this.x / s, this.y / s);
+    }
+
+    normalize() {
+        let l = this.length();
+        this.x /= l;
+        this.y /= l;
+    }
+
+    limit(l) {
+        if (this.length() > l) {
+            this.normalize();
+            this.mult(l);
+        }
+    }
+
+    dot(v) {
+        if (v instanceof Vector)
+            return this.x * v.x + this.y * v.y;
+    }
+
+    angleTo(v) {
+        if (v instanceof Vector) {
+            let a = Math.abs(v.angle() - this.angle());
+            if (a > pi)
+                a = 2*pi - a;
+            return a;
+        }
     }
 }
--- script.js
+++ script.js
@@ -16,15 +16,14 @@ ctx.font = "50px Georgia";
 let canvasPosition = canvas.getBoundingClientRect();
 
 const mouse = {
-    x: canvas.width/2,
-    y: canvas.height/2,
+    p: new Vector(canvas.width/2, canvas.height/2),
     click: false
 }
 
 canvas.addEventListener("mousedown", function (event) {
     mouse.click = true;
-    mouse.x = event.x - canvasPosition.left;
-    mouse.y = event.y - canvasPosition.top;
+    mouse.p.x = event.x - canvasPosition.left;
+    mouse.p.y = event.y - canvasPosition.top;
 });
 canvas.addEventListener("mouseup", function () {
     mouse.click = false;
@@ -34,8 +33,6 @@ canvas.addEventListener("mouseup", function () {
 class Player {
     constructor() {
         this.p = new Vector(canvas.width/2, canvas.height/2);
-        this.x = canvas.width/2;
-        this.y = canvas.height/2;
         this.radius = 80;
         this.angle = 0;
         this.frameX = 0;
@@ -45,32 +42,25 @@ class Player {
         this.spriteHeight = 10;
     }
     update() {
-        const dx = mouse.x - this.x;
-        const dy = mouse.y - this.y;
-        if (dx !== 0 && dy !== 0) {
-            this.angle = Math.acos(dx / Math.sqrt(dx*dx + dy*dy)); // dot product with (1|0)
-            if (dy < 0)
-                this.angle *= -1;
+        const d = mouse.p.getSub(this.p.x, this.p.y);
+        if (d.length() > 1) {
+            this.angle = d.angle();
         }
-        if (dx !== 0) {
-            this.x += dx/25;
-        }
-        if (dy !== 0) {
-            this.y += dy/25;
-        }
+        d.divXY(25, 24);
+        this.p.add(d.x, d.y);
     }
     draw() {
         if (mouse.click) {
             ctx.lineWidth = 0.3;
             ctx.beginPath();
-            ctx.moveTo(this.x, this.y);
-            ctx.lineTo(mouse.x, mouse.y);
+            ctx.moveTo(this.p.x, this.p.y);
+            ctx.lineTo(mouse.p.x, mouse.p.y);
             ctx.stroke();
         }
         ctx.fillStyle = "orange";
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, this.angle + pi/8, this.angle + pi*15/8);
-        ctx.lineTo(this.x, this.y);
+        ctx.arc(this.p.x, this.p.y, this.radius, this.angle + pi/8, this.angle + pi*15/8);
+        ctx.lineTo(this.p.x, this.p.y);
         ctx.closePath();
         ctx.fill();
     }
